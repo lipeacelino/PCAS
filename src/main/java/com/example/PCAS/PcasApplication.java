d351ecbdc2f62af6f03bcc4d0ef75cadade63e69
Felipe Acelino <felipe.acelino@dcx.ufpb.br>
2021-09-04 13:38:53 -0300

    Primeiro commit

---
+++ src/main/java/com/example/PCAS/PcasApplication.java
@@ -0,0 +1,13 @@
+package com.example.PCAS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PCAS/PcasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PCAS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
